7a1ca894dfc61ff0f21bb43221f35a4e48da7053
morikentarou <routatarou1119@gmail.com>
2024-05-13 02:37:11 +0900

    サーバルーティンを調整

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   
   before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index 
     @prototypes = Prototype.includes(:user)
